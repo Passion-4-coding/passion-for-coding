9876ace4c376e77575dc96495d17a83519b07151
Rudin Alexandr <alexandr.rudin.my@gmail.com>
2021-08-20 15:14:30 +0200

    update adapter version

--- svelte.config.js
+++ svelte.config.js
@@ -6,8 +6,8 @@ const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
-	adapter: adapter(),
 	kit: {
+		adapter: adapter(),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
