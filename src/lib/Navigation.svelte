24e7a8bf02df3e2c96e1a086fca6cd79b341abc1
Rudin Alexandr <alexandr.rudin.my@gmail.com>
2022-05-23 22:52:25 +0200

    change navigation

--- src/lib/Navigation.svelte
+++ src/lib/Navigation.svelte
@@ -2,20 +2,26 @@
   import { page } from '$app/stores';
 </script>
 
-<ul class="navigation" role="navigation">
-  <li class="nav-item" title="Join">
-    <a href="https://discord.gg/zbMKUhSU7r">Join</a>
-  </li>
-  <li class="nav-item" class:active={$page.url.pathname === '/contact'} title="Contact">
-    <a href="/contact">Contact</a>
-  </li>
-  <li class="nav-item" class:active={$page.url.pathname === '/members'} title="Members">
-    <a href="/members">Members</a>
-  </li>
-</ul>
+<nav class="navigation">
+  <a class="nav-item" href="https://discord.gg/zbMKUhSU7r">Join</a>
+  <a
+    class="nav-item"
+    class:active={$page.url.pathname === '/contact'}
+    title="Contact"
+    href="/contact">
+      Contact
+  </a>
+  <a
+    class="nav-item"
+    class:active={$page.url.pathname === '/members'}
+    title="Members"
+    href="/members">
+      Members
+  </a>
+</nav>
 
 <style>
-  ul.navigation {
+  nav.navigation {
     list-style-type: none;
     padding-left: 0;
     display: flex;
