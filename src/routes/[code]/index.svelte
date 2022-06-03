88ad8c7a76bb69a1df289a55b4f84b96acbe6981
Rudin Alexandr <alexandr.rudin.my@gmail.com>
2022-06-03 21:21:37 +0200

    fix meta tag

--- src/routes/[code]/index.svelte
+++ src/routes/[code]/index.svelte
@@ -7,7 +7,8 @@
 
 <svelte:head>
 	<title>Passion for coding</title>
-	<meta name="description" content="Svelte demo app" />
+	<meta name="description" content="Passion for coding, discord community for developers">
+	<meta name="keywords" content="Discord community, passion for coding, freelance, upwork, coding, programming, developers">
 </svelte:head>
 
 <HomeHero />
