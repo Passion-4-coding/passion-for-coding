4eeb2940a5500cfbdf78163445452fb91ad635e1
Tirimar <dkrasilnikov2002@gmail.com>
2022-12-30 01:13:43 +0300

    Roles page layout

---
+++ src/lib/components/RoleBlock.svelte
@@ -0,0 +1,101 @@
+<script lang="ts">
+  export let title: string;
+  export let description: string;
+  export let imgName: string;
+</script>
+
+<!-- layout -->
+
+<div class="role-block">
+  <img class="role-icon" src={`/images/${imgName}.png`} alt={title + " icon"} />
+  <div class="role-info">
+    <span class="role-title">{title}</span>
+    <span class="role-description">{description}</span>
+  </div>
+</div>
+
+<!-- styles -->
+
+<style>
+  .role-block {
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+    border: 5px solid var(--color-secondary);
+    text-align: left;
+  }
+  .role-icon {
+    margin: 0 23px 0 18px;
+    object-fit: scale-down;
+    max-width: 108px;
+    max-height: 108px;
+  }
+  .role-info {
+    display: flex;
+    flex-direction: column;
+    line-height: 25px;
+    margin: 20px 10px 20px 0;
+  }
+  .role-title {
+    font-weight: 600;
+    font-size: 30px;
+    color: var(--color-primary);
+  }
+  .role-description {
+    font-size: 24px;
+    margin-top: 10px;
+  }
+  @media (max-width: 992px) {
+    .role-info {
+      margin: 15px 10px 15px 0;
+      line-height: 19px;
+    }
+    .role-title {
+      font-weight: 600;
+      font-size: 23px;
+      color: var(--color-primary);
+    }
+    .role-description {
+      font-size: 20px;
+      margin-top: 10px;
+    }
+    .role-icon {
+      margin: 8px 13px 8px 7px;
+      object-fit: fill;
+      max-width: 80px;
+      max-height: 80px;
+    }
+  }
+
+  @media (max-width: 600px) {
+    .role-block {
+      border: 3px solid var(--color-secondary);
+      border-top-width: 1.5px;
+      border-bottom-width: 1.5px;
+      width: 100%;
+      display: flex;
+    }
+    .role-icon {
+      margin: 8px 13px 8px 7px;
+      object-fit: scale-down;
+      max-width: 40px;
+      max-height: 40px;
+    }
+    .role-info {
+      display: flex;
+      flex-direction: column;
+      margin: 0 7px 10px 0;
+    }
+    .role-title {
+      line-height: 25px;
+      font-weight: 600;
+      font-size: 20px;
+      color: var(--color-primary);
+    }
+    .role-description {
+      margin-top: 0;
+      font-size: 16px;
+      line-height: 12px;
+    }
+  }
+</style>
--- src/routes/[code]/roles.svelte
+++ src/routes/[code]/roles.svelte
@@ -1,28 +1,68 @@
-<script context="module" lang="ts">
-  import type { Load } from "@sveltejs/kit";
+<script>
+  import Footer from "$lib/components/Footer.svelte";
+  import RoleBlock from "$lib/components/RoleBlock.svelte";
+  import Title from "$lib/components/Title.svelte";
+  import {useTranslations} from "$lib/modules/translations";
 
-  export const load: Load = ({ params }) => {
-    return {
-      props: {
-
-      }
-    }
-  }
+  const { t } = useTranslations("roles");
 </script>
 
-<div class="content">
-  Page is under development
+<!-- layout -->
+
+<div class="roles container">
+  <Title>{t("title")}</Title>
+  <div class="roles-content">
+    <RoleBlock title="Service" description={t("service")} imgName="role0000" />
+    <RoleBlock title="Trainee" description={t("trainee")} imgName="role0001" />
+    <RoleBlock title="Junior" description={t("junior")} imgName="role0010" />
+    <RoleBlock title="Middle" description={t("middle")} imgName="role0011" />
+    <RoleBlock title="Senior" description={t("senior")} imgName="role0100" />
+    <RoleBlock title="Principal" description={t("principal")} imgName="role0101" />
+    <RoleBlock title="Architect" description={t("architect")} imgName="role0110" />
+    <RoleBlock title="Lead" description={t("lead")} imgName="role1110" />
+    <RoleBlock title="Owner" description={t("owner")} imgName="role1111" />
+  </div>
 </div>
 
+<Footer />
+
+<!-- styles -->
+
 <style>
-  .content {
-    height: 100%;
+  .roles {
     display: flex;
-    align-items: center;
     justify-content: center;
-    font-size: 56px;
-    line-height: 34px;
+    flex-direction: column;
+    padding: 100px 20px 70px 20px;
+  }
+  .roles-content {
+    margin-top: 50px;
+    display: grid;
+    grid-auto-flow: column;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: 1fr 1fr 1fr;
+    grid-gap: 20px 40px;
+  }
+  @media (max-width: 992px) {
+    .roles {
+      padding: 50px 15px;
+    }
+    .roles-content {
+      grid-auto-flow: row;
+      grid-template-columns: 1fr 1fr;
+      grid-gap: 10px 20px;
+    }
+  }
+  @media (max-width: 600px) {
+    .roles {
+      padding: 40px 10px;
+    }
+    .roles-content {
+      margin-top: 10px;
+
+      grid-template-rows: none;
+      grid-template-columns: 1fr;
+      grid-gap: 0;
+    }
   }
 </style>
-
-<!-- markup (zero or more items) goes here -->
